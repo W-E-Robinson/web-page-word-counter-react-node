aa78fcf0c5de66fff50b7806152e1c4bce8de35f
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-01 20:47:39 +0100

    added test for Redux actions for GET /wordCount

---
+++ src/modules/Redux/actions/wordCount/__tests__/index.test.ts
@@ -0,0 +1,27 @@
+import {
+    fetchWordCountRequest,
+    fetchWordCountFailure,
+    fetchWordCountSuccess,
+} from "../actions";
+
+describe("Actions - GET /dishes", () => {
+    test("fetchWordCountRequest", () => {
+        expect(fetchWordCountRequest({ url: "mock url" }).type).toBe("FETCH_WORD_COUNT_REQUEST");
+    });
+    test("fetchWordCountFailure", () => {
+        expect(fetchWordCountFailure({ error: "mock error" }).type).toBe("FETCH_WORD_COUNT_FAILURE");
+    });
+    test("fetchWordCountSuccess", () => {
+        expect(fetchWordCountSuccess({
+            webPageInfo: {
+                webPageUrl: "mock url",
+                totalWordCount: 10,
+                destructuredWordCount: [
+                    { word: "the", count: 100 },
+                    { word: "and", count: 90 },
+                    { word: "umbrella", count: 2 },
+                ],
+            },
+        }).type).toBe("FETCH_WORD_COUNT_SUCCESS");
+    });
+});
