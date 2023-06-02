17fa58dc351f61e97c9feddfa36ba1919fb73935
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-03 00:09:20 +0100

    extended heading using typography + added initial heading styling

--- src/pages/WebCount/index.tsx
+++ src/pages/WebCount/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+import Typography from "@mui/material/Typography";
+
+import styles from "./styles.module.sass";
+
 export const WordCount = () => {
     return (
-        <>
-            <h1>Heading</h1>
-        </>
+        <div className={styles["container"]}>
+            <div className={styles["header"]}>
+                <Typography variant="h4">Web Page Word Counter</Typography>
+                <Typography variant="h6">SITE_DESCRIPTION</Typography>
+            </div>
+        </div>
     );
 };
