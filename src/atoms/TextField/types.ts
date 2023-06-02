aa1b1de6e4fcf367506faf4588dfeaaf57957133
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-02 11:33:45 +0100

    added text field atom

---
+++ src/atoms/TextField/types.ts
@@ -0,0 +1,10 @@
+import { ChangeEvent } from "react";
+
+export interface TextFieldProps {
+    id: string;
+    value: string;
+    label: string;
+    onChange: (event: ChangeEvent) => void;
+    error?: boolean;
+    helperText?: string | null;
+}
