4b84898c500631c2a01d6a37e927c6fac09b956f
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-03 12:45:19 +0100

    finished testing for WordCount page

--- src/pages/WebCount/components/Header.tsx
+++ src/pages/WebCount/components/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
         <div className={styles["container"]}>
             <div className={styles["header"]}>
                 <Typography variant="h4">Web Page Word Counter</Typography>
-                <Typography variant="h6">Paste in a URL in to the form below and click Count to find out how many words are in the page!</Typography>
+                <Typography variant="h6">Paste a URL into the form below and click Count to find out how many words are in the page!</Typography>
             </div>
         </div>
     );
