6e506eaba6f0b5cf7d7f247fb5ca8e40229f595b
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-03 09:43:55 +0100

    added Suspensed accordion to page

--- src/pages/WebCount/index.tsx
+++ src/pages/WebCount/index.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
 
+import { useSelector } from "react-redux";
+
+import { AppState } from "../../modules/Redux/reducers/rootReducer";
 import { Header } from "./components/Header";
 
+const Accordion = React.lazy(() => import("../../organisms/Accordion").then(module => ({ default: module.Accordion })));
+
 export const WordCount = () => {
+    const { wordCountsInfo } = useSelector((state: AppState) => state.wordCounts);
+
     return (
         <>
             <Header />
+            {wordCountsInfo.length &&
+                <Suspense fallback={<></>}>
+                    <Accordion accordionData={wordCountsInfo} />
+                </Suspense>}
         </>
     );
 };
