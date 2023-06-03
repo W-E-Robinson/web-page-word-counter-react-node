6159adad5f785613fa058a8bdb97194e1337ba89
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-03 15:36:24 +0100

    updated url to match the current greeter hello service provided by molecular js

--- src/constants/endpoints/index.ts
+++ src/constants/endpoints/index.ts
@@ -1,3 +1,3 @@
-const BACKEND_PORT = process.env.BACKEND_PORT;
+export const BACKEND_PORT = process.env.BACKEND_PORT ?? "http://localhost:3000/api";
 
 export const WORD_COUNT_URL = `${BACKEND_PORT}/wordCount`;
--- src/modules/Redux/apis/wordCount/index.ts
+++ src/modules/Redux/apis/wordCount/index.ts
@@ -1,11 +1,14 @@
 import axios, { AxiosError } from "axios";
+import { BACKEND_PORT } from "../../../../constants/endpoints";
 
-import { WORD_COUNT_URL } from "../../../../constants/endpoints/index";
+//import { WORD_COUNT_URL } from "../../../../constants/endpoints/index";
 
 export const getWordCount = async (
     webPageUrl: string,
 ) => {
-    const url = `${WORD_COUNT_URL}?webPageUrl=${webPageUrl}`;
+    //const url = `${WORD_COUNT_URL}?webPageUrl=${webPageUrl}`;
+    console.info(webPageUrl);
+    const url = `${BACKEND_PORT}/greeter/hello`;
 
     try {
         const response = await axios.get(url);
