f11d9520409461d6fe5479452bed886c1dbcfe45
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-04 12:50:08 +0100

    added with env file backend port + update to gitignore and readme

--- src/constants/endpoints/index.ts
+++ src/constants/endpoints/index.ts
@@ -1,3 +1,3 @@
-const BACKEND_PORT = process.env.BACKEND_PORT ?? "http://localhost:3000/api";
+const BACKEND_PORT = `http://localhost:${process.env.REACT_APP_BACKEND_PORT_NUMBER}/api`;
 
 export const COUNTER_SERVICE = `${BACKEND_PORT}/counter`;
