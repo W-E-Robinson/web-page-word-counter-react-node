e298bb57099db757601a0051f1f5a711f1b10a3d
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-18 19:25:29 +0100

    added error boundary test

---
+++ src/modules/ErrorBoundary/__tests__/index.test.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+
+import { ErrorBoundary } from "../index";
+
+describe("ErrorBoundary testing", () => {
+    const Child = () => {
+        throw new Error();
+    };
+
+    test("Error text renders on component tree error", () => {
+        render(
+            <ErrorBoundary>
+                <Child />
+            </ErrorBoundary>,
+        );
+
+        expect(screen.getByText("Sorry, there was an error.")).toBeInTheDocument();
+    });
+});
