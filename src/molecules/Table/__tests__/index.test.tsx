be4b5c1c64d5fb8d5ce2f55b01369613f2f9e3c8
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-19 18:31:18 +0100

    updated test data and row test + started pagination testing

--- src/molecules/Table/__tests__/index.test.tsx
+++ src/molecules/Table/__tests__/index.test.tsx
@@ -8,8 +8,18 @@ describe("Table testing", () => {
     const mockProps = {
         headers: ["Word", "Count"],
         rows: [
-            { word: "the", count: 100 },
-            { word: "hello", count: 3 },
+            { word: "first", count: 1000 },
+            { word: "second", count: 600 },
+            { word: "third", count: 200 },
+            { word: "fourth", count: 100 },
+            { word: "fifth", count: 90 },
+            { word: "sixth", count: 8 },
+            { word: "seventh", count: 5 },
+            { word: "eigth", count: 4 },
+            { word: "ninth", count: 4 },
+            { word: "tenth", count: 3 },
+            { word: "eleventh", count: 3 },
+            { word: "twelth", count: 1 },
         ],
     };
 
@@ -28,9 +38,26 @@ describe("Table testing", () => {
             rows={mockProps.rows}
         />);
 
-        expect(screen.getByText("the")).toBeInTheDocument();
-        expect(screen.getByText("hello")).toBeInTheDocument();
+        expect(screen.getByText("first")).toBeInTheDocument();
+        expect(screen.getByText(1000)).toBeInTheDocument();
+        expect(screen.getByText("second")).toBeInTheDocument();
+        expect(screen.getByText(600)).toBeInTheDocument();
+        expect(screen.getByText("third")).toBeInTheDocument();
+        expect(screen.getByText(200)).toBeInTheDocument();
+        expect(screen.getByText("fourth")).toBeInTheDocument();
         expect(screen.getByText(100)).toBeInTheDocument();
+        expect(screen.getByText("fifth")).toBeInTheDocument();
+        expect(screen.getByText(90)).toBeInTheDocument();
+    });
+    xtest("pagination", async () => {
+        render(<Table
+            headers={mockProps.headers}
+            rows={mockProps.rows}
+        />);
+
+        expect(screen.getByText("first")).toBeInTheDocument();
+        expect(screen.getByText(1000)).toBeInTheDocument();
+        expect(screen.getByText("second")).toBeInTheDocument();
         expect(screen.getByText(3)).toBeInTheDocument();
     });
 });
