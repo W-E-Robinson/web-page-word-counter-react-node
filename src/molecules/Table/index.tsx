186c2e261a72871acbe14642b85b09bb10d22401
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-19 18:19:23 +0100

    updated table to have pagination

--- src/molecules/Table/index.tsx
+++ src/molecules/Table/index.tsx
@@ -1,10 +1,11 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 
 import { Table as MUITable } from "@mui/material";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
@@ -14,6 +15,18 @@ export const Table = memo(({
     headers,
     rows,
 }: TableProps) => {
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    const handleChangePage = (_event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
     return (
         <TableContainer component={Paper}>
             <MUITable aria-label="table">
@@ -25,14 +38,25 @@ export const Table = memo(({
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row) => (
-                        <TableRow key={Object.keys(row)[0]}>
-                            {Object.values(row).map((cell) => {
-                                return <TableCell>{cell}</TableCell>;
-                            })}
-                        </TableRow>
-                    ))}
+                    {rows
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((row) => (
+                            <TableRow key={Object.keys(row)[0]}>
+                                {Object.values(row).map((cell) => {
+                                    return <TableCell>{cell}</TableCell>;
+                                })}
+                            </TableRow>
+                        ))}
                 </TableBody>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={rows.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
             </MUITable>
         </TableContainer>
     );
