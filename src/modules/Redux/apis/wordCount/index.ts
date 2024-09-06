478acfbdd7c7db1368aa2208f936251c62b2f04d
Will Robinson <william.ellis.robinson@gmail.com>
2024-09-06 08:40:40 +0100

    fix: updated app to work with new web page word counter node

--- src/modules/Redux/apis/wordCount/index.ts
+++ src/modules/Redux/apis/wordCount/index.ts
@@ -3,12 +3,11 @@ import axios, { AxiosError } from 'axios';
 import { COUNTER_SERVICE } from '../../../../constants/endpoints/index';
 
 export const getWordCount = async (
-    webPageUrl: string,
+    url: string,
 ) => {
-    const url = `${COUNTER_SERVICE}/count?webPageUrl=${webPageUrl}`;
-
     try {
-        const response = await axios.get(url);
+        const response = await axios.get(`${COUNTER_SERVICE}/count?url=${url}`);
+        console.log(response.data);
         return response.data;
     } catch (error) {
         const axiosError = error as AxiosError;
